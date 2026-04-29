4cebfd89d948222decba45f040888418a61438b4
Guy <guyskv@gmail.com>
2026-04-29 17:09:06 +0300

    Initial Commit

---
+++ Class26B-ios-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Class26B-ios-1
+//
+//  Created by Guy Macin on 29/04/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
